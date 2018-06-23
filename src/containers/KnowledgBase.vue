27e00637a191d40de0cdd8748568eeffe81720a9
Gabriel <gabrielmartin@Gabriels-MacBook-Pro.local>
2018-06-23 15:04:27 -0700

    check in

--- src/containers/KnowledgBase.vue
+++ src/containers/KnowledgBase.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="container column" id="knowledge-base">
-        <h1 class="heading">Knowledge Base</h1>
-        <hr class="divider">
-        <div class="lists-container column">
-            <div class="knowledge-base-card column">
-                <h2 class="sub-heading">Languages</h2>
-                <ul class="knowledge-base-list row">
-                    <li>HTML5</li>
-                    <li>CSS3</li>
-                    <li>JAVASCRIPT</li>
-                </ul>
-            </div>
+  <div class="container column" id="knowledge-base">
+    <h1 class="heading">Knowledge Base</h1>
+    <hr class="divider">
+    <div class="lists-container column">
+      <div class="knowledge-base-card column">
+        <h2 class="sub-heading">Languages</h2>
+        <ul class="knowledge-base-list row">
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JAVASCRIPT</li>
+        </ul>
+      </div>
 
-            <div class="knowledge-base-card column">
-                <h2 class="sub-heading">Frameworks</h2>
-                <ul class="knowledge-base-list row">
-                    <li>React</li>
-                    <li>Vue</li>
-                </ul>
-            </div>
+      <div class="knowledge-base-card column">
+        <h2 class="sub-heading">Frameworks</h2>
+        <ul class="knowledge-base-list row">
+          <li>React</li>
+          <li>Vue</li>
+        </ul>
+      </div>
 
-            <div class="knowledge-base-card column">
-                <h2 class="sub-heading">Interests</h2>
-                <ul class="knowledge-base-list row">
-                    <li>Ruby on Rails</li>
-                    <li>Python</li>
-                    <li>React Native</li>
-                    <li>...rest</li>
-                </ul>
-            </div>
-
-        </div>
-        <div>
-            <p>I'm always eager to expand my capabilities. While my focus is to become more proficient in javscript, I'm still exloring other tools and languages. I'm never apposed to learning and growing in my profession. Please reach out if you're interested in sharing some wisdom! Contact links are below.</p>
-        </div>
+      <div class="knowledge-base-card column">
+        <h2 class="sub-heading">Interests</h2>
+        <ul class="knowledge-base-list row">
+          <li>Ruby on Rails</li>
+          <li>Python</li>
+          <li>React Native</li>
+          <li>...rest</li>
+        </ul>
+      </div>
 
     </div>
+    <div>
+      <p>I'm always eager to expand my capabilities. While my focus is to become more proficient in javscript, I'm still exloring other tools and languages. I'm never apposed to learning and growing in my profession. Please reach out if you're interested in sharing some wisdom! Contact links are below.</p>
+    </div>
+
+  </div>
 </template>
 
 <script>
