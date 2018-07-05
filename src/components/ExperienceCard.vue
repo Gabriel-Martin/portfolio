4eabd9001e778113a826cbd45262fff1326291e2
Gabriel <gabrielmartin@gabriels-mbp.lan>
2018-07-05 10:14:53 -0700

    Fixed cursor on ExperienceCard hover.

--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="experience-card row" onmouseover="" v-in-viewport.once="-200">
+  <div class="experience-card row" v-in-viewport.once="-200">
     <div class="card-heading">
       <h3 class="name"> {{ name }} </h3>
       <p class="vertical-bar">|</p>
@@ -16,92 +16,91 @@
 </template>
 
 <script>
-export default {
-  props: {
-    name: String,
-    type: String,
-    summary: String,
-    githubLink: String,
-    demoLink: String
-  }
-};
+  export default {
+    props: {
+      name: String,
+      type: String,
+      summary: String,
+      githubLink: String,
+      demoLink: String
+    }
+  };
 </script>
 
 <style scoped>
-.experience-card {
-  opacity: 0;
-  padding: 1rem;
-  margin-bottom: 2rem;
-  align-items: center;
-  color: var(--deepPurple);
-  border-radius: 0 15px 0 15px;
-  transform: translateX(-100px);
-  transition: all 0.2s ease-in-out;
-  background-color: var(--light-text);
-}
-
-.in-viewport {
-  opacity: 1;
-  transform: translateX(0px);
-}
-
-.experience-card:hover,
-.experience-card:visited {
-  cursor: pointer;
-  border-radius: 0;
-  transform: scale(1.02);
-  box-shadow: 10px 10px 20px -7px #1c1d23;
-}
-
-.name,
-.type,
-.summary,
-.vertical-bar,
-.link-container,
-.summary-container {
-  margin: 0;
-}
-
-.vertical-bar {
-  font-size: 0.8rem;
-}
-
-.card-heading {
-  min-width: 9rem;
-  text-align: center;
-  margin-right: 1rem;
-}
-
-.name,
-.type {
-  font-weight: 100;
-}
-
-.summary-container {
-  flex: 1;
-  padding: 1rem;
-  border-radius: 15px 0 5px 0;
-  background-color: var(--dark-orange);
-}
-
-.summary-container,
-.experience-link {
-  color: var(--light-text);
-}
-
-.experience-link {
-  text-decoration: none;
-  border-bottom: 1px solid var(--light-text);
-}
-
-.link-container {
-  white-space: nowrap;
-  text-align: right;
-}
-
-@media screen and (max-width: 600px) {
   .experience-card {
-    flex-direction: column;
+    opacity: 0;
+    padding: 1rem;
+    margin-bottom: 2rem;
+    align-items: center;
+    color: var(--deepPurple);
+    border-radius: 0 15px 0 15px;
+    transform: translateX(-100px);
+    transition: all 0.2s ease-in-out;
+    background-color: var(--light-text);
   }
-}
+
+  .in-viewport {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+
+  .experience-card:hover,
+  .experience-card:visited {
+    border-radius: 0;
+    transform: scale(1.02);
+    box-shadow: 10px 10px 20px -7px #1c1d23;
+  }
+
+  .name,
+  .type,
+  .summary,
+  .vertical-bar,
+  .link-container,
+  .summary-container {
+    margin: 0;
+  }
+
+  .vertical-bar {
+    font-size: 0.8rem;
+  }
+
+  .card-heading {
+    min-width: 9rem;
+    text-align: center;
+    margin-right: 1rem;
+  }
+
+  .name,
+  .type {
+    font-weight: 100;
+  }
+
+  .summary-container {
+    flex: 1;
+    padding: 1rem;
+    border-radius: 15px 0 5px 0;
+    background-color: var(--dark-orange);
+  }
+
+  .summary-container,
+  .experience-link {
+    color: var(--light-text);
+  }
+
+  .experience-link {
+    text-decoration: none;
+    border-bottom: 1px solid var(--light-text);
+  }
+
+  .link-container {
+    white-space: nowrap;
+    text-align: right;
+  }
+
+  @media screen and (max-width: 600px) {
+    .experience-card {
+      flex-direction: column;
+    }
+  }
 </style>
